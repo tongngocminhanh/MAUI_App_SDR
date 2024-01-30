7501214e94e1556a46fa59252bd02117c40b2a01
AnhTong <tongngocminhanh.102@gmail.com>
2024-01-30 14:03:16 +0100

    Unsupported packages in net 6.0, update to net 8.0

---
+++ MySEProject/AppSDR/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace AppSDR.ViewModel
+{
+    public partial class MainViewModel
+    { 
+    }
+}
