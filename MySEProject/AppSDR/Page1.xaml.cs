5f0713f50e8b50b3dbd1409adb808410ecc1271f
AnhTong <tongngocminhanh.102@gmail.com>
2024-02-26 12:53:59 +0100

    Update drawing function of TienSon into AppSDR

--- MySEProject/AppSDR/Page1.xaml.cs
+++ MySEProject/AppSDR/Page1.xaml.cs
@@ -2,8 +2,19 @@ namespace AppSDR;
 
 public partial class Page1 : ContentPage
 {
-	public Page1()
-	{
-		InitializeComponent();
-	}
+    public readonly int[][] vectors;
+    public Page1(int[][] activeCellsColumn)
+    {
+        InitializeComponent();
+
+        vectors = activeCellsColumn;
+        var drawable = new GraphicsDrawable();
+        drawable.Vectors = vectors;
+        Content = new GraphicsView
+        {
+            Drawable = drawable
+        };
+
+
+    }
 }
